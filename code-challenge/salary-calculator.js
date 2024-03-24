f81b41728255ff56828dad42051d78187dbc5431
Gideon Macharia <kamaugedion95@gmail.com>
2024-03-24 20:19:33 +0300

    detect speed

--- code-challenge/salary-calculator.js
+++ code-challenge/salary-calculator.js
@@ -1,2 +1,26 @@
+let basicSalary
+let minimumWage = 24000
+let tax
+let salaryMax = [32333,500000 ,800000]
 
+
+function salaryCalculator(basicSalary) {
+    if(basicSalary === minimumWage) {
+        tax = basicSalary * 0.1
+        return tax
+    }else if(basicSalary > minimumWage && basicSalary <= salaryMax[0]) {
+        tax = basicSalary * 0.25
+        return tax
+    }else if(basicSalary > value1 && basicSalary <= salaryMax[1]) {
+        tax= basicSalary * 0.3
+        return tax
+    }else if(basicSalary > value2 && basicSalary <= salaryMax[2]) {
+        tax = basicSalary * 0.325
+        return tax
+    }else if (basicSalary > value3) {
+        tax = basicSalary * 0.35
+        return tax
+    }
+}
     
+salaryCalculator(500000)
